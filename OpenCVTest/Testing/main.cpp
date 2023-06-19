032c2168a969a52e0d3d30ed30f7036130dfba1d
KieferAtkin <kieferatkin91@gmail.com>
2023-06-19 22:17:43 +0100

    Checkpoint

--- OpenCVTest/Testing/main.cpp
+++ OpenCVTest/Testing/main.cpp
@@ -8,29 +8,23 @@
 using namespace cv;
 using namespace std;
 
-/********** back up hough ********/
-//int inverseRatio = 1;
-//int hough_Min_Threshold = 33;
-//int hough_Max_Threshold = 180;
-//int hough_Min_Distance = 16;
-//int hough_min_Radius = 50;
-//int hough_max_Radius = 150;
 
-/* pupil boundry */
-double pupil_inverseRatio = 1.0;
-double pupil_param_1 = 30; // to do with Canny.
-double pupil_param_2 = 50; // accuracy : lower is less good
-double pupil_min_dist_div = 8; // For some reason the lower the number the better the result. Could result in false.
-int pupil_min_radius = 50;
-int pupil_max_radius = 200;
 
-/* iris boundry */
-double iris_inverseRatio = 1.0;
-double param_1 = 30; // to do with Canny.
-double param_2 = 50; // accuracy : lower is less good
-double iris_min_dist_div = 8; // For some reason the lower the number the better the result. Could result in false.
-int min_radius = 100;
-int max_radius = 300;
+/* Hough Pupil Boundry */
+double pupil_InverseRatio = 1.0;
+double pupil_Param1 = 75; // to do with Canny.
+double pupil_Param2 = 50; // Threshold for the circle centers at the detection stage ( lower = more likely to have false circles).
+double pupil_MinDistDiv = 24; // Minimum distance between the centers of the detected circles.
+int pupil_MinRadius = 50;
+int pupil_MaxRadius = 200;
+
+/* Hough Iris Boundry */
+double iris_InverseRatio = 1.0;
+double iris_Param1 = 38; // specifies the max threshold value it will use for its canny. then Min is half the max.
+double iris_Param2 = 67; // Threshold for the circle centers at the detection stage ( lower = more likely to have false circles).
+double iris_MinDistDiv = 24; // Minimum distance between the centers of the detected circles.
+int iris_MinRadius = 120;
+int iris_MaxRadius = 250;
 
 cv::Size GetResolution(const cv::Mat& image)
 {
@@ -42,7 +36,7 @@ cv::Size GetResolution(const cv::Mat& image)
 
 static void LoadImageSource(cv::Mat& imageSource)
 {
-    std::string imagePath = "Resources/EyeSignImages/TestImages2.bmp";
+    std::string imagePath = "Resources/EyeSignImages/TestImages4.bmp";
 
     // Load image wether its colour or not.
     imageSource = cv::imread(imagePath, cv::IMREAD_COLOR);
@@ -78,16 +72,18 @@ void detectAndDrawCircles(Mat& imgSource, Mat& imgDilated, float inverseRatio, i
         Vec3i circleParams = detectedCircles[circleIndex];
         Point center = Point(circleParams[0], circleParams[1]); // [0] = Xcenter, [1] = Ycenter
 
-        // Draw a small circle at the center of the current circle
-        // using the specified color with a thickness of 2 pixels
-        circle(imgSource, center, 1, circleColor, 2, LINE_AA);
+        // Draw a center point for the current circle
+        // using the specified colour with a thickness of 1px.
+        int lineThickness = 1;
+        int centerPointRadius = 1;
+        circle(imgSource, center, centerPointRadius, circleColor, lineThickness, LINE_AA);
 
         // Extract the radius of the current circle
         int radius = circleParams[2]; // [2] = the radius.
 
         // Draw the outline of the current circle
         // using the center, radius, and specified color with a thickness of 2 pixels
-        circle(imgSource, center, radius, circleColor, 2, LINE_AA);
+        circle(imgSource, center, radius, circleColor, lineThickness, LINE_AA);
 
         PrintMessages::PrintCircleCoordinates(circleType, center.x, center.y, radius);
     }
@@ -115,57 +111,97 @@ cv::Mat calculateGradient(const cv::Mat& input)
     return gradient;
 }
 
-cv::Mat imageProcessing(cv::Mat imgSource)
+cv::Mat performDilate(const cv::Mat& input)
+{
+
+    cv::Mat dilatedImage;
+    cv::Mat structuringElement = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(2, 2));
+
+    cv::dilate(input, dilatedImage, structuringElement);
+
+    return dilatedImage;
+}
+
+cv::Mat imageProcessing(cv::Mat imgSource, cv::Size blurKernelSize, double blurSigmaX, double blurSigmaY, 
+                        int cannyThreshold1, int cannyThreshold2, int sobelApetureSize, bool dilate)
 {
     cv::Mat imgGaussianBlur;
     cv::Mat imgGradient;
     cv::Mat imgCanny;
 
     // Apply Gaussian blur to the grayscale image
-    cv::GaussianBlur(imgSource, imgGaussianBlur, cv::Size(5, 5), 1.4, 0);
-    imshow("Gaussian", imgGaussianBlur);
+    cv::GaussianBlur(imgSource, imgGaussianBlur, blurKernelSize, blurSigmaX, blurSigmaY);
+    //imshow("Gaussian", imgGaussianBlur);
 
     // Calculate gradient using the intensity gradient filter
     imgGradient = calculateGradient(imgGaussianBlur);
 
     // Convert the gradient image to 8-bit
     imgGradient.convertTo(imgGradient, CV_8U);
-    imshow("Gradient", imgGradient);
+    //imshow("Gradient", imgGradient);
 
     // Perform Canny edge detection on the gradient image
-    cv::Canny(imgGaussianBlur, imgCanny, 0, 75, 3);
-    imshow("Canny", imgCanny);
+    cv::Canny(imgGaussianBlur, imgCanny, cannyThreshold1, cannyThreshold2, sobelApetureSize);
+    imshow("canny*", imgCanny);
 
-   return imgCanny;
+
+    // Perform dilation if the flag is set to true
+    if (dilate) 
+    {
+        imgCanny = performDilate(imgCanny);
+    }
+    
+    return imgCanny;
 }
 
 void main()
 {
     Mat imgLoaded, imgGray;
 
+    // load the Source image.
     LoadImageSource(imgLoaded);
     cv::Mat imgSource = imgLoaded;
    
-    // Convert the source image to grayscale
+    // Convert the source image to grayscale.
     cvtColor(imgSource, imgGray, COLOR_BGR2GRAY);
 
 
-    // Call the imageProcessing function to process the image
-    cv::Mat processedImage = imageProcessing(imgGray);
+    cv::Size pupil_BlurKernelSize(5, 5);
+    double pupil_blurSigmaX = 1.4;
+    double pupil_blurSigmaY = 0; // if 0 then it uses the same as SigmaX
+    int pupil_CannyThreshold1 = 0;
+    int pupil_CannyThreshold2 = 75;
+    int pupil_sobelApetureSize = 3;
     
-    // Call the function for the "Pupil" circles
-    detectAndDrawCircles(imgSource, processedImage, pupil_inverseRatio,
-                         pupil_min_dist_div, pupil_param_1, pupil_param_2, 
-                         pupil_min_radius, pupil_max_radius, "Pupil", Scalar(0, 255, 255));
 
-    // Call the function for the "Limbic" circles
-    detectAndDrawCircles(imgSource, processedImage, iris_inverseRatio,
-                         iris_min_dist_div, param_1, param_2, 
-                         min_radius, max_radius, "Limbic", Scalar(255, 0, 255));
+    // Process the image for pupil boundry
+    cv::Mat processedImage = imageProcessing(imgGray, pupil_BlurKernelSize, pupil_blurSigmaX, pupil_blurSigmaY, 
+                                             pupil_CannyThreshold1, pupil_CannyThreshold2, pupil_sobelApetureSize, false);
+    //imshow("Canny1", processedImage );
+    
+    // Detect and draw the pupil boundry.
+    detectAndDrawCircles(imgSource, processedImage, pupil_InverseRatio,
+                         pupil_MinDistDiv, pupil_Param1, pupil_Param2, 
+                         pupil_MinRadius, pupil_MaxRadius, "Pupil", Scalar(0, 255, 255));
+
+    
+    cv::Size iris_BlurKernelSize(5,5);
+    double iris_blurSigmaX = 1.4;
+    double iris_blurSigmaY = 0; // if 0 then it uses the same as SigmaX
+    int iris_CannyThreshold1 = 15;
+    int iris_CannyThreshold2 = 38;
+    int iris_sobelApetureSize = 3;
+    // Process the image for Limbic Boundry.
+    cv::Mat processedImage2 = imageProcessing(imgGray, iris_BlurKernelSize, iris_blurSigmaX, iris_blurSigmaY, 
+                                                iris_CannyThreshold1, iris_CannyThreshold2, iris_sobelApetureSize, false);
+    imshow("Canny2", processedImage2);
+
+    // Detect and draw the limbic Boundry..
+    detectAndDrawCircles(imgSource, processedImage2, iris_InverseRatio,
+                         iris_MinDistDiv, iris_Param1, iris_Param2, 
+                         iris_MinRadius, iris_MaxRadius, "Limbic", Scalar(255, 0, 255));
+
 
     imshow("detected circles", imgSource);
-    
-    
     waitKey(0);
-
 }
